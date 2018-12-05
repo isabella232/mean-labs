f9292354a2a90444e3e0becd114fbddf2ca6f4de
Gimpan <jimmyquach@live.com>
2018-12-05 15:50:50 +0100

    Added get notes function in the service

--- src/app/labnotebook/labnotebook.service.ts
+++ src/app/labnotebook/labnotebook.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 import { environment } from '../../environments/environment';
 import { Labnote } from './labnote.model';
@@ -9,8 +11,32 @@ const BACKENDURL = environment.apiUrl + '/labnotebook/';
 
 @Injectable({providedIn: 'root'})
 export class LabNoteBookService {
-
+  private notes: Labnote[] = [];
+  private notesUpdated = new Subject<{notes: Labnote [], noteCount: number}>();
 constructor (private http: HttpClient, private router: Router) {}
+
+getNotes(notesPerPage: number, currentNotes: number) {
+  const queryParams = `?pagesize=${notesPerPage}&page=${currentNotes}`;
+  this.http.get<{message: string, notes: any, maxNotes: number}>(BACKENDURL + queryParams)
+  .pipe(map((notesData) => {
+    return {
+      notes: notesData.notes.map(note => {
+        return {
+          title: note.title,
+          content: note.content,
+          id: note._id,
+          date: note.date
+        };
+    }), maxNotes: notesData.maxNotes};
+  }))
+    .subscribe((transformedPostData) => {
+      this.notes = transformedPostData.notes;
+      this.notesUpdated.next({
+        notes: [...this.notes],
+        noteCount: transformedPostData.maxNotes
+      });
+    });
+}
 
   getNote(id: string) {
     return this.http.get<{
@@ -18,7 +44,7 @@ constructor (private http: HttpClient, private router: Router) {}
     title: string,
     content: string,
     date: Date
-    }>('BACKENDURL' + id);
+    }>(BACKENDURL + id);
   }
 
   addNote(title: string, content: string) {
@@ -26,7 +52,6 @@ constructor (private http: HttpClient, private router: Router) {}
     noteData.append('title', title);
     noteData.append('content', content);
 
-    const noteDataJSON = { title: title, content: content};
     this.http.post<{message: string, note: Labnote}>(BACKENDURL, noteData)
     .subscribe((responseData) => {
       this.router.navigate(['/labnotebook/labnotes']);
