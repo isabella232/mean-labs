0879c54a921cdff2459b4d7a7e1fa1e6ef418e97
Gimpan <jimmyquach@live.com>
2019-01-16 17:20:30 +0100

    Added some images for demo

--- src/app/demo/gallery-demo/gallery-demo/gallery-demo.component.ts
+++ src/app/demo/gallery-demo/gallery-demo/gallery-demo.component.ts
@@ -11,18 +11,63 @@ export class GalleryDemoComponent implements OnInit {
   pictures = [
     {
       id: 1,
-      title: 'Recipe Book',
-      img: 'https://gimpan.com/img/koreanburger.jpg'
+      title: 'Snow cat',
+      img: 'https://gimpan.com/img/cat-snow.jpg'
     },
     {
       id: 2,
-      title: 'Roadmap',
-      img: '../../assets/1.jpg'
+      title: 'Korean Burger',
+      img: 'https://gimpan.com/img/koreanburger.jpg'
     },
     {
       id: 3,
-      title: 'Gallery',
+      title: 'Dog',
       img: 'https://gimpan.com/img/dog.jpeg'
+    },
+    {
+      id: 4,
+      title: 'Origami',
+      img: 'https://gimpan.com/img/origamipikachu.jpg'
+    },
+    {
+      id: 5,
+      title: 'The one ring',
+      img: 'https://gimpan.com/img/lotrring.jpg'
+    },
+    {
+      id: 6,
+      title: 'Space',
+      img: 'https://gimpan.com/img/space.jpg'
+    },
+    {
+      id: 7,
+      title: 'Abstract',
+      img: 'https://gimpan.com/img/abstractred.jpg'
+    },
+    {
+      id: 8,
+      title: 'Ocean floor',
+      img: 'https://gimpan.com/img/oceanfloor.jpg'
+    },
+    {
+      id: 9,
+      title: 'Flower',
+      img: 'https://gimpan.com/img/purpleflowers.jpg'
+    },
+    {
+      id: 10,
+      title: 'Spider',
+      img: 'https://gimpan.com/img/spider.jpg'
+    },
+    {
+      id: 11,
+      title: 'White horse',
+      img: 'https://gimpan.com/img/whitehorse.jpg'
+    },
+    {
+      id: 12,
+      title: 'Coffee',
+      img: 'https://gimpan.com/img/coffee.jpeg'
     }
   ];
   ngOnInit() {
